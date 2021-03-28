c9131cf5d8684f1597a959029a755f6a56dcc59f
Mateusz Nowak <nowak-m@outlook.com>
2021-03-28 11:28:10 +0200

    add basic PushEvents observable

--- we-push/src/app/serv/github-api.service.ts
+++ we-push/src/app/serv/github-api.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 
 export interface GithubEventActor {
   avatar_url: string;
@@ -35,9 +37,48 @@ export interface GithubPushEvent extends GithubEvent {
 export class GithubApiService {
   private eventsUrl = `https://api.github.com/events`;
 
-  events$ = this.http.get<GithubEvent[]>(this.eventsUrl, {
-    observe: 'response'
-  });
+  private apiAvailable = true;
+
+  private apiInterval = '';
+
+  private convertInterval(intervalInSeconds: string): number {
+    return +intervalInSeconds * 1000;
+  }
+
+  events$: Observable<GithubEvent[]> = of('').pipe(
+    switchMap(() => {
+      if (this.apiAvailable) {
+        this.apiAvailable = false;
+        return this.http
+          .get<GithubEvent[]>(this.eventsUrl, {
+            observe: 'response'
+          })
+          .pipe(
+            map((response) => {
+              this.apiInterval = response.headers.get('X-Token') || '60';
+              return response.body || ([] as GithubEvent[]);
+            }),
+            tap(() => {
+              setTimeout(() => {
+                this.apiAvailable = true;
+              }, this.convertInterval(this.apiInterval));
+            })
+          );
+      } else {
+        console.error(
+          `Github API is locked for ${this.apiInterval} seconds, try again later.`
+        );
+        return of([] as GithubEvent[]);
+      }
+    })
+  );
+
+  pushEvents$: Observable<GithubPushEvent[]> = this.events$.pipe(
+    map(
+      (events) =>
+        events.filter((e) => e.type == 'PushEvent') as GithubPushEvent[]
+    )
+  );
 
   constructor(public http: HttpClient) {}
 }
