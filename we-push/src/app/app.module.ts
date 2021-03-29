d901228f48e697aea00e227627f778916bd1d994
Mateusz Nowak <nowak-m@outlook.com>
2021-03-29 11:21:26 +0200

    add Controls component

--- we-push/src/app/app.module.ts
+++ we-push/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventDisplayComponent } from './comp/event-display/event-display.component';
+import { ControlsComponent } from './comp/controls/controls.component';
 
 @NgModule({
-  declarations: [AppComponent, EventDisplayComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, EventDisplayComponent, ControlsComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
