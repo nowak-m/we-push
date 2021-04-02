ea48202f023c764b9f182ea1e8f358fcf712012c
Mateusz Nowak <nowak-m@outlook.com>
2021-04-02 10:22:06 +0200

    Cleanup templates Remove unused outlet variable

--- we-push/src/app/app.component.ts
+++ we-push/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { filter, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { NavigationStart, Router, RouterOutlet } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
 import { WelcomeDialogComponent } from './comp/welcome-dialog/welcome-dialog.component';
 import { slider } from './animations/route-animations';
 
@@ -16,8 +16,6 @@ export class AppComponent implements OnInit {
   title = 'we-push';
 
   navigationStart$: Observable<string>;
-
-  outlet!: RouterOutlet;
 
   constructor(public dialog: MatDialog, public router: Router) {
     this.navigationStart$ = this.router.events.pipe(
